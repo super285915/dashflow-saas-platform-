d8d7b32651689110fbf6744acafdf581bbcd0baf
super285915 <seniordev285915@gmail.com>
2025-05-19 20:27:25 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
   },
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'images.pexels.com'],
+    unoptimized: true, // Add this for Netlify compatibility
   },
+  output: 'standalone', // Add this for better Netlify support
 };
 
 module.exports = nextConfig;
